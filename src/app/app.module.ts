ad9b9cfafbb93681f48255dff2c57ba22ab65102
Anett Oravec <60674947+Annie2900@users.noreply.github.com>
2023-09-01 00:36:32 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,19 @@ const appRoutes: Routes = [
   { path: 'intervju', component: ListingComponent  },
   { path: 'tbn', component: ListingComponent  },
   { path: 'panoramica', component: ListingComponent  },
-  { path: 'search', component: ListingComponent  },
+  { path: 'latest/details/:id', component: DetailsComponent },
+  { path: 'najava/details/:id', component: DetailsComponent  },
+  { path: 'zajednica/details/:id', component: DetailsComponent  },
+  { path: 'kultura/details/:id', component: DetailsComponent  },
+  { path: 'sport/details/:id', component: DetailsComponent  },
+  { path: 'privreda/details/:id', component: DetailsComponent  },
+  { path: 'video/details/:id', component: DetailsComponent  },
+  { path: 'dailyphoto/details/:id', component: DetailsComponent  },
+  { path: 'press/details/:id', component: DetailsComponent  },
+  { path: 'intervju/details/:id', component: DetailsComponent  },
+  { path: 'tbn/details/:id', component: DetailsComponent  },
+  { path: 'panoramica/details/:id', component: DetailsComponent  },
+  { path: 'details/:tab/:id', component: DetailsComponent }
 ];
 
 @NgModule({
